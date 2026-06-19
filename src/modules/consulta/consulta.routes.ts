f223ed4d47545e986037f1c35ff92c5f5bdc2d07
Rosme-J227 <rosmejc.23456@gmail.com>
2026-06-18 20:15:44 -0400

    fix: tipar routers para evitar error TS2883 (#3)
    Esta bien, comprendo si vi los cambios

--- src/modules/consulta/consulta.routes.ts
+++ src/modules/consulta/consulta.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-const router = Router();
+const router: Router = Router();
 
 // TODO: Implementar rutas de consulta diagnóstica e historial (RF-13 a RF-19)
 // router.post("/", ConsultaController.consultar);
