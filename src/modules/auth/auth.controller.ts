11f9c113a566987c0f5d46b06558f993d647b9bb
PabloDJesus7Ca <pabloiaspasiago@icloud.com>
2026-06-22 22:13:27 -0400

    feat: Archivo Inicial Contolador De Auth.

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,12 +1,34 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response } from "express";
+import { AuthService } from "./auth.service";
 
 export class AuthController {
   // TODO: Implementar controlador de login (RF-01, RF-02)
-  static async login(_req: Request, res: Response, next: NextFunction) {
+  static async loginOfUserFromController(request: Request, response: Response) {
     try {
-      res.status(501).json({ message: "No implementado" });
-    } catch (error) {
-      next(error);
+      const VerifyUser = await AuthService.CheckLoginUserFromService(request.body);
+      if (VerifyUser === true) {
+        return response.status(200).json({ message: "Haz Iniciado Session Correctamente" });
+      }
+      return response.status(401).json({ message: "Usuario No Autorizado" });
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        return response.status(404).json({ message: "Este Usario No Se Encontro" });
+      }
+      return response.status(500).json({ message: "Error Desconocido" });
+    }
+  }
+
+  static async RegisterNewUserFromController(request: Request, response: Response) {
+    try {
+      const NewUser = await AuthService.createNewUserFromService(request.body);
+      return response.status(201).json({ NewUser });
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        return response.status(400).json({
+          message: error.message,
+        });
+      }
+      return response.status(500).json({ message: "Unknow error" });
     }
   }
 }
