52c99ea604938f70edc4c5f22831ae89cf8aa3c5
PabloDJesus7Ca <pabloiaspasiago@icloud.com>
2026-06-22 22:19:32 -0400

    feat: Archivo Tipo Authrepositorio Hacia La Base De Datos Donde Se Almacena La Conexion De La DBD.

---
+++ src/Shared/Errors/LoginError.ts
@@ -0,0 +1,10 @@
+export class UserResponses extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "UserResponses";
+
+    Object.setPrototypeOf(this, UserResponses.prototype);
+  }
+}
+
+
