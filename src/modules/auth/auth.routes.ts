851f89819f536220f400dabc450fd37cf716f062
PabloDJesus7Ca <pabloiaspasiago@icloud.com>
2026-06-22 22:23:09 -0400

    feat: Nuevos Endpoints Fueron Agregados Inicialmente El de Login Y El De Register

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
-
+import { AuthController } from "./auth.controller";
 const router: Router = Router();
 
 // TODO: Implementar endpoint POST /api/auth/login (RF-01, RF-02)
-// router.post("/login", AuthController.login);
-
+router.post("/login", AuthController.loginOfUserFromController);
+router.post("/register", AuthController.RegisterNewUserFromController);
 export default router;
